d037e3ba13f771610c409076a2a7caf766b3c771
Иван <kirillovio@mail.ru>
2025-01-13 21:37:59 +0300

    настроил CRUD ля Product

--- online_store/urls.py
+++ online_store/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from . import views
-
+from online_store.views import ProductCreateView, ProductUpdateView, ProductDeleteView, ProductListView, ProductDetailView
 app_name = 'online_store'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+
     path('contacts', views.contacts, name='contacts'),
-    path('categories', views.categories, name='categories'),
-    path('orders', views.orders, name='orders'),
-    path('pattern', views.pattern, name='pattern'),
-    path('index', views.index, name='index'),
+
+
+    path('product/', ProductListView.as_view(), name='product_list'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('product/new/', ProductCreateView.as_view(), name='product_create'),
+    path('product/<int:pk>/edit/', ProductUpdateView.as_view(), name='product_edit'),
+    path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 ]
--- online_store/views.py
+++ online_store/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import  CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from online_store.models import Product, Category
 
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ['name', 'description', 'category', 'purchase_price', 'preview']
+    #template_name = 'product_form.html'
+    success_url = reverse_lazy('online_store:product_list')
 
-def home(request):
-    products = Product.objects.all
-    context = {
-        'products': products,
-    }
-    return render(request, 'online_store/home.html', context)
+class ProductListView(ListView):
+    model = Product
+    #template_name = 'product_list.html'
+    context_object_name = 'products'
+
+class ProductDetailView(DetailView):
+    model = Product
+    #template_name = 'product_detail.html'
+    context_object_name = 'product'
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ['name', 'description', 'category', 'purchase_price', 'preview']
+    success_url = reverse_lazy('online_store:product_list')
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    #template_name = 'product_confirm_delete.html'
+    success_url = reverse_lazy('online_store:product_list')
 
 def contacts(request):
     if request.method == "POST":
@@ -19,25 +40,3 @@ def contacts(request):
         return HttpResponse(f"Спасибо {name}!")
 
     return render(request, 'online_store/contacts.html')
-
-def categories(request):
-    сategorys = Category.objects.all
-    context = {
-        'сategorys': сategorys,
-    }
-    return render(request, 'online_store/categories.html',context)
-
-def orders(request):
-    return render(request, 'online_store/orders.html')
-
-def pattern(request):
-    return render(request, 'online_store/pattern.html')
-def index(request):
-    product = Product.objects.get(id=9)
-    context = {
-        'product': product,
-        'name': product.name,
-        'description': product.description,
-        'preview': product.preview,
-    }
-    return render(request, 'online_store/index.html', context)
