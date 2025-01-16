220f97d9ea26eef6684b94c5ee725c058056ae9f
Иван <kirillovio@mail.ru>
2025-01-16 21:26:55 +0300

    настройка приложение blogs

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from blogs.models import Publications
+
+
+@admin.register(Publications)
+class PublicationsAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'updated_at', 'publication_flag', 'number_of_views',)
+    list_filter = ('updated_at', 'publication_flag')
+    search_fields = ('title', 'content',)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import  CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from blogs.models import Publications
 
-# Create your views here.
+class PublicationsCreateView(CreateView):
+    model = Publications
+    fields = ['title', 'content', 'preview', 'publication_flag']
+    #template_name = 'Publications_form.html'
+    success_url = reverse_lazy('blogs:publications_list')
+
+class PublicationsListView(ListView):
+    model = Publications
+    #template_name = 'Publications_list.html'
+    context_object_name = 'publications'
+
+class PublicationsDetailView(DetailView):
+    model = Publications
+    #template_name = 'Publications_detail.html'
+    context_object_name = 'publications'
+
+class PublicationsUpdateView(UpdateView):
+    model = Publications
+    fields = ['title', 'content', 'preview', 'publication_flag']
+    success_url = reverse_lazy('blogs:publications_list')
+
+class PublicationsDeleteView(DeleteView):
+    model = Publications
+    #template_name = 'Publications_confirm_delete.html'
+    success_url = reverse_lazy('blogs:publications_list')
